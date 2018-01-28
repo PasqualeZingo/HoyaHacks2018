63c7b487f36ca506bd584b3a8cb7a16ad7f22026
Jackson Jorss <jjorss@sevone.com>
2018-01-27 19:33:17 -0500

    integrated firebase; send and receive current conversation

--- app/src/components/conversation-manager/index.jsx
+++ app/src/components/conversation-manager/index.jsx
@@ -1,34 +1,60 @@
 import React, { Component } from 'react';
 import InputManager from '../input-manager/index';
 import Conversation from '../conversation/index';
+import request from 'request';
+import firebase from '../../firebase';
 
 export default class ConversationManager extends Component {
     
     constructor(props) {
         super(props);
         this.state = {
-            messages: [
-                {
-                    user: 'computer',
-                    text: 'Hello world, I am a computer',
-                    time: new Date().toString()
-                }
-            ]
+            messages: []
         };
     }
 
+    componentDidMount() {
+        const conRef = firebase.database().ref('Users/UserName/conversation/messages/');
+        conRef.on('value', function(snap) {
+            const messagesObject = snap.val();
+            const keys = Object.keys(messagesObject);
+            let messages = this.state.messages;
+            keys.forEach((key) => {
+                messages.push(messagesObject[key]);
+            });
+            this.setState({
+                messages: messages
+            });
+        }.bind(this));
+    }
+
     createMessage = (user: string, text: string) => {
-        let messages = this.state.messages;
-        let date = new Date();
-        messages.push({
+        const date = new Date();
+        const message = {
             user: user,
             text: text,
             time: `${date.toLocaleTimeString()}  ${date.toLocaleDateString()}`
-        });
-        this.setState({
-            messages: messages
-        });
+        }
+        this.handleOnSubmit(message);
     } 
+
+    handleOnSubmit = (message: object) => {
+        var options = 
+        { 
+            method: 'POST',
+            url: 'https://us-central1-hoyahacks2018.cloudfunctions.net/app/message',
+            headers: 
+            { 
+                'Content-Type': 'application/json'
+            },
+            body: message,
+            json: true
+        };
+
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+        });
+    }
 
     render() {
         return(
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,24 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+// middleware
+app.use(bodyParser.json());
+app.use(cors({ origin: true }));
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+app.post('/message', (req, res) => {
+    console.log('made it here');
+    const message = req.body;
+    const key = admin.database().ref('/Users/').child(message.user).child('/conversations/')
+        .child('uniquekey/').child('messages/').push().key;
+    return admin.database().ref('/Users/').child(message.user).child('/conversation/')
+        .child('messages/').child(key).set(message).then(()=>{
+            return res.status(200).end();
+        });
+});
+
+
+exports.app = functions.https.onRequest(app);
