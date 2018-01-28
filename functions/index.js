1d545dfd4b13f62fd8dff9c4056f2e5e096c90ce
Jackson Jorss <jjorss@sevone.com>
2018-01-28 02:50:21 -0500

    fix cool stuff

--- functions/index.js
+++ functions/index.js
@@ -15,35 +15,45 @@ app.use(cors({ origin: true }));
 app.post('/message', (req, res) => {
         console.log('made it here');
         const message = req.body;
+        if(message.user === 'user') {
+          // do request
+        }
         const key = admin.database().ref('/Users/').child(message.user).child('/conversations/').child('uniquekey/').child('messages/').push().key;
-        return admin.database().ref('/Users/').child(message.user).child('/conversation/').child('messages/').child(key).set(message).then(()=>{
-                var tone_analyzer = new ToneAnalyzerV3({
-                        username: '3b4be1ed-7fb7-4df0-9642-dd12906ee300',
-                        password: 'pBbC6UECVqIO',
-                        version_date: '2017-09-21'
-                    });
-                var params = {
-                    'tone_input': {"text" : req.body.message},
-                    'content_type': 'application/json'
-                };
-		console.log("here comes tone");
-                return tone_analyzer.tone(params, function(error, response) {
-                        if (error)
-			    {    console.log('error:', error);
-				return error;
-			    }
-                        else
-			    {
-				console.log(JSON.stringify(response, null, 2));
-				return JSON.stringify(response, null, 2);
-			    }
-                    });
-            }, err =>{
-		console.log(err);
-            }).then((watson)=>{
-                    return res.status(200).end();
-	    });
+        return admin.database().ref('/Users/')
+          .child(message.user).child('/conversation/')
+            .child('messages/').child(key).set(message).then(()=>{
+              return res.status(200).end();
+            });      
 });
+
+let getTone = () => {
+  let tone_analyzer = new ToneAnalyzerV3({
+    username: '3b4be1ed-7fb7-4df0-9642-dd12906ee300',
+    password: 'pBbC6UECVqIO',
+    version_date: '2017-09-21'
+  });
+  let params = {
+    'tone_input': {"text" : req.body.message},
+    'content_type': 'application/json'
+  };
+
+  // return tone_analyzer.tone(params, function(error, response) {
+  //   if (error) {
+  //     console.log('error:', error);
+  //     return error;
+  //   } else {
+  //     //console.log(JSON.stringify(response, null, 2));
+  //     return JSON.stringify(response, null, 2);
+  //   }
+  // });
+//   , err =>{
+//     console.log(err);
+//     }).then((watson)=>{
+//   return res.status(200).end();
+//   });
+// }
+}
+
 
 
 app.post('/startConvo', (req, res) => {
@@ -65,16 +75,23 @@ app.post('/startConvo', (req, res) => {
           endConversation = true;
         } else {
           // Display the output from dialog, if any.
-          if (!!response.output.text.length != 0) {
-            return res.send(response.output.text[0]);
+          if (!!response.output.text.length !== 0) {
+            const message = {
+              text: response.output.text[0],
+              user: 'computer',
+              time: '123'
+            }
+          const key = admin.database().ref('/Users/').child(message.user).child('/conversations/').child('uniquekey/').child('messages/').push().key;
+          return admin.database().ref('/Users/').child(message.user)
+            .child('/conversation/').child('messages/').child(key).set(message).then(()=>{
+              return res.status(200).end();
+            });
           }
         }
       }
     return (watson.startConversation(processResponse));
 });
 app.post('/handleReply', (req, res) => {
-    //res.send("hi :c");
-    
     let processResponse = (err, response) => {
         if (err) {
           console.error(err); // something went wrong
@@ -92,8 +109,17 @@ app.post('/handleReply', (req, res) => {
           endConversation = true;
         } else {
           // Display the output from dialog, if any.
-          if (!!response.output.text.length != 0) {
-            return res.send(response.output.text[0]);
+          if (!!response.output.text.length !== 0) {
+            const message = {
+              text: response.output.text[0],
+              user: 'computer',
+              time: '123'
+            }
+            const key = admin.database().ref('/Users/').child(message.user).child('/conversations/').child('uniquekey/').child('messages/').push().key;
+            return admin.database().ref('/Users/').child(message.user)
+              .child('/conversation/').child('messages/').child(key).set(message).then(()=>{
+                return res.status(200).end();
+              });
         }
         }
       }
